bd8ead76e217b0dd40f3d2d617f8d8833bd8f241
Aasif <aasifali4@gmail.com>
2023-01-09 18:12:43 +0000

    first commit

---
+++ src/Components/Products.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch,useSelector } from 'react-redux';
+import { add } from '../Store/cartSlice';
+import {fetchProducts} from '../Store/productSlice';
+import { STATUSES } from '../Store/productSlice';
+
+
+const Products = () => {
+    const dispatch = useDispatch();
+    const {data: products, status} = useSelector((stste)=>stste.product);
+    // const [products, setproducts] = useState([]);
+
+    useEffect(() => {
+        dispatch(fetchProducts());
+        // const fetchProducts = async () => {
+        //     const res = await fetch('https://fakestoreapi.com/products');
+        //     const data = await res.json();
+        //     console.log(data)
+        //     setproducts(data);
+
+        // }
+        // fetchProducts()
+
+    }, []);
+    const handleAdd=(product)=>{
+        dispatch(add(product))
+
+    }
+    if(status===STATUSES.LOADING){
+        return <h2>Loading...</h2>;
+    }
+    if(status===STATUSES.ERROR){
+        return <h2>Something went wrong!</h2>
+    }
+    return (
+        <div className='productsWrapper'>
+            {
+                products.map(product => (
+                    <div className='card' key={product.id}>
+                        <img src={product.image} alt="" />
+                        <h4>{product.title}</h4>
+                        <h5>{product.price}</h5>
+                        <button onClick={()=>handleAdd(product)} className='btn'>Add to cart</button>
+                    </div>
+                ))
+            }
+
+        </div>
+    )
+}
+
+export default Products
